538f6fdbde09ab6985448899b92f312e3d36a4ad
S-A-Mahamud <sahariaalmahamudbd@gmail.com>
2026-02-23 01:02:18 +0600

    Add dynamic job counter functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello Job Tracker!');
+let interviewList = [];
+let rejectedList = [];
+
+const totalJobsElement = document.querySelectorAll(".jobCount");
+const interviewJobsElement = document.getElementById("interviewJobs");
+const rejectedJobsElement = document.getElementById("rejectedJobs");
+
+const allCardsConttainer = document.getElementById("allCardsContainer");
+
+function updateJobCounters() {
+    totalJobsElement.forEach(element => {
+        element.textContent = allCardsConttainer.children.length;
+    });
+    interviewJobsElement.textContent = interviewList.length;
+    rejectedJobsElement.textContent = rejectedList.length;
+}
+
+// Initialize counters
+updateJobCounters();
