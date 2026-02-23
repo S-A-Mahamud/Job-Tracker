c384eaa0c0b56059cff0a0f7756fafbb29979d49
S-A-Mahamud <sahariaalmahamudbd@gmail.com>
2026-02-23 07:58:34 +0600

    Add toggle active button

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function updateJobCounters() {
 
 // Initialize counters
 updateJobCounters();
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+
+function toggleActiveButton(activeButton) {
+    const filterButtons = [allFilterBtn, interviewFilterBtn, rejectedFilterBtn];
+
+    filterButtons.forEach(button => {
+        // all active class remove
+        button.classList.remove("bg-blue-500");
+
+        // all default class add
+        button.classList.add("bg-gray-500");
+    });
+
+    // add class for active button
+    activeButton.classList.remove("bg-gray-500");
+    activeButton.classList.add("bg-blue-500");
+}
