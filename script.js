41c92fd7d2c6a71c3733e4b2dfece43d1885c598
S-A-Mahamud <sahariaalmahamudbd@gmail.com>
2026-02-24 18:24:30 +0600

    Add some styles

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ const filteredJobsSection = document.getElementById("filteredJobsSection");
 
 
 function updateJobCounters() {
-
     const totalJobs = allCardsContainer.children.length;
 
     // Header total count
@@ -36,11 +35,11 @@ function updateJobCounters() {
     }
     else if (currentFilter === "interview") {
         selectedJobText.innerHTML =
-            `<span class="font-bold">${interviewList.length}</span> of ${totalJobs} jobs`;
+            `<span class="font-bold text-green-600">${interviewList.length}</span> of <span class="font-bold">${totalJobs}</span> jobs`;
     }
     else if (currentFilter === "rejected") {
         selectedJobText.innerHTML =
-            `<span class="font-bold">${rejectedList.length}</span> of ${totalJobs} jobs`;
+            `<span class="font-bold text-red-600">${rejectedList.length}</span> of <span class="font-bold">${totalJobs}</span> jobs`;
     }
 }
 
@@ -61,7 +60,7 @@ function toggleActiveButton(activeButton) {
 
     // add class for active button
     activeButton.classList.remove("bg-gray-500");
-    activeButton.classList.add("bg-blue-500");
+    activeButton.classList.add("bg-blue-500", "hover:bg-blue-600");
 
     // Show/hide sections based on active button
     if (activeButton.id == 'interview-filter-btn') {
@@ -86,6 +85,7 @@ function toggleActiveButton(activeButton) {
 
 // Event listener for main section to handle interview and reject button clicks
 mainContainer.addEventListener("click", function (event) {
+
     // Check if the clicked element is an interview or reject button
     if (event.target.classList.contains("interview-btn")) {
         const card = event.target.closest(".job-card");
@@ -98,12 +98,19 @@ mainContainer.addEventListener("click", function (event) {
         card.querySelector(".status").textContent = "INTERVIEW";
 
         // after updating filtered card
-        const originalCard = Array.from(allCardsContainer.children).find(c =>
-            c.querySelector(".company-name").textContent === companyName &&
-            c.querySelector(".designation").textContent === designation
-        );
+        let originalCard = null;
+
+        for (const c of allCardsContainer.children) {
+            const name = c.querySelector(".company-name").textContent;
+            const desig = c.querySelector(".designation").textContent;
+
+            if (name === companyName && desig === designation) {
+                originalCard = c;
+                break;
+            }
+        }
         if (originalCard) {
-            originalCard.querySelector(".status").textContent = "INTERVIEW";
+            originalCard.querySelector(".status").innerHTML = `<button class="status bg-green-200 font-bold text-green-500">INTERVIEW</button>`;
         }
 
         const cardInfo = {
@@ -135,13 +142,20 @@ mainContainer.addEventListener("click", function (event) {
 
         card.querySelector(".status").textContent = "REJECTED";
 
-        // after updating filtered card
-        const originalCard = Array.from(allCardsContainer.children).find(c =>
-            c.querySelector(".company-name").textContent === companyName &&
-            c.querySelector(".designation").textContent === designation
-        );
+        let originalCard = null;
+
+        for (const c of allCardsContainer.children) {
+            const name = c.querySelector(".company-name").textContent;
+            const desig = c.querySelector(".designation").textContent;
+
+            if (name === companyName && desig === designation) {
+                originalCard = c;
+                break;
+            }
+        }
+
         if (originalCard) {
-            originalCard.querySelector(".status").textContent = "REJECTED";
+            originalCard.querySelector(".status").innerHTML = `<button class="status bg-red-200 font-bold text-red-500 ">REJECTED</button>`;
         }
 
         const cardInfo = {
@@ -204,14 +218,16 @@ mainContainer.addEventListener("click", function (event) {
 }
 );
 
+
+
 // Function to create job cards based on the interviewList
 function createInterviewJobCard() {
     filteredJobsSection.innerHTML = ""; // Clear previous cards
 
     if (interviewList.length === 0) {
         filteredJobsSection.innerHTML = `
-        <div class="text-center mt-18 border bg-white p-6 rounded-lg">
-            <img src="./jobs.png" alt="No Rejected Jobs" class="mx-auto w-24 opacity-50">
+        <div class="text-center mt-18 border border-green-600 bg-white p-6 rounded-lg">
+            <img src="./jobs.png" alt="No Interview Jobs" class="mx-auto w-24 opacity-50">
         <h2 class="text-center mt-4">No jobs available</h2>
         <p class="text-center mt-2 text-gray-500">Check back soon for new job opportunities</p>
         </div> `;
@@ -221,25 +237,78 @@ function createInterviewJobCard() {
     for (const interview of interviewList) {
 
         let div = document.createElement("div");
-        div.classList = 'job-card flex flex-col md:flex-row justify-between border p-4 bg-white rounded-lg mt-4';
+        div.classList.add('job-card', 'border', 'border-green-600', 'p-4', 'bg-white', 'rounded-lg', 'hover:shadow-xl',
+            'hover:shadow-green-500/40',
+            'hover:scale-[1.02]',
+            'transition-all',
+            'duration-300',
+            'mb-4');
         div.innerHTML = `
-            <div class="space-y-4">
-                        <h3 class="company-name text-2xl font-semibold">${interview.companyName}</h3>
+            <div class="space-y-2">
+                          <div class="flex items-center justify-between gap-4">
+                            <h3 class="company-name text-2xl font-semibold">${interview.companyName}</h3>
+
+                            <div class="relative group inline-block">
+                                <button
+                                    class="delete-btn bg-gray-200 text-red-500 font-bold p-2 cursor-pointer mt-0 rounded-lg">
+                                    <i class="delete-btn fa-solid fa-trash 
+                                    transform 
+                                    hover:scale-150 
+                                    transition-transform 
+                                    duration-300 
+                                    ease-in-out"></i>
+                                </button>
+
+                                <span class="absolute top-0.5 right-2.5 -translate-x-1/2 
+                              bg-gray-200 text-red-600 font-bold text-xs
+                                px-2 py-2 rounded-xl
+                                opacity-0 group-hover:opacity-100
+                                transition-all duration-300
+                                whitespace-nowrap">
+
+                                    Delete Job
+
+                                    <!-- Tooltip Arrow -->
+                                    <span class="absolute left-full top-1/2 -translate-y-1/2 -translate-x-1/2
+                                    w-2.5 h-2.5 bg-gray-200
+                                    rotate-45"></span>
+                                </span>
+                            </div>
+                        </div>
                         <p class="designation font-light">${interview.designation}</p>
                         <p class="salary font-light">${interview.salary}</p>
-                        <button class="status bg-gray-100 p-2 font-bold">${interview.status}</button>
+                         
+                         <button class="status bg-green-200 font-bold text-green-500 p-2 rounded">${interview.status}</button>
                         <p class="details font-medium">${interview.details}</p>
                         <div class="flex gap-2 mt-2">
                             <button
-                                class="interview-btn text-green-600 border border-green-600 text-lg py-2 px-2 rounded cursor-pointer">INTERVIEW</button>
+                                class="interview-btn  text-lg py-2 px-2 rounded cursor-pointer
+                                relative overflow-hidden  
+                                border border-green-500 text-green-600 font-semibold
+                                transition duration-300 
+                                before:absolute before:inset-0
+                              before:bg-green-100
+                                before:origin-left
+                                before:scale-x-0
+                                before:transition-transform
+                                before:duration-300
+                                hover:before:scale-x-100
+                                before:-z-10 
+                                ">INTERVIEW</button>
                             <button
-                                class="reject-btn text-red-600 border border-red-600 text-lg py-2 px-2 rounded cursor-pointer">REJECTED</button>
+                                class="reject-btn text-lg py-2 px-2 rounded cursor-pointer                              
+                                relative overflow-hidden  
+                                border border-red-500 text-red-600 font-semibold
+                                transition duration-300 
+                                before:absolute before:inset-0
+                              before:bg-red-100
+                                before:origin-right
+                                before:scale-x-0
+                                before:transition-transform
+                                before:duration-300
+                                hover:before:scale-x-100
+                                before:-z-10">REJECTED</button>
                         </div>
-                    </div>
-                    
-                     <div>
-                        <button
-                            class="delete-btn bg-red-500 text-white font-bold py-2 px-4 rounded cursor-pointer mt-4 md:mt-0">Delete</button>
                     </div>
     `;
         filteredJobsSection.appendChild(div);
@@ -253,7 +322,7 @@ function createRejectedJobCard() {
 
     if (rejectedList.length === 0) {
         filteredJobsSection.innerHTML = `
-        <div class="text-center mt-18 border bg-white p-6 rounded-lg">
+        <div class="text-center mt-18 border border-red-600 bg-white p-6 rounded-lg">
             <img src="./jobs.png" alt="No Rejected Jobs" class="mx-auto w-24 opacity-50">
         <h2 class="text-center mt-4">No jobs available</h2>
         <p class="text-center mt-2 text-gray-500">Check back soon for new job opportunities</p>
@@ -265,25 +334,77 @@ function createRejectedJobCard() {
     for (const rejected of rejectedList) {
 
         let div = document.createElement("div");
-        div.classList = 'job-card flex flex-col md:flex-row justify-between border p-4 bg-white rounded-lg mt-4';
+        div.classList.add('job-card', 'border', 'border-red-600', 'p-4', 'bg-white', 'rounded-lg', 'hover:shadow-xl',
+            'hover:shadow-red-500/40',
+            'hover:scale-[1.02]',
+            'transition-all',
+            'duration-300',
+            'mb-4');
         div.innerHTML = `
-            <div class="space-y-4">
-                        <h3 class="company-name text-2xl font-semibold">${rejected.companyName}</h3>
+            <div class="space-y-2">
+                          <div class="flex items-center justify-between gap-4">
+                            <h3 class="company-name text-2xl font-semibold">${rejected.companyName}</h3>
+
+                            <div class="relative group inline-block">
+                                <button
+                                    class="delete-btn bg-gray-200 text-red-500 font-bold p-2 cursor-pointer mt-0 rounded-lg">
+                                    <i class="delete-btn fa-solid fa-trash 
+                                    transform 
+                                    hover:scale-150 
+                                    transition-transform 
+                                    duration-300 
+                                    ease-in-out"></i>
+                                </button>
+
+                                <span class="absolute top-0.5 right-2.5 -translate-x-1/2 
+                              bg-gray-200 text-red-600 font-bold text-xs
+                                px-2 py-2 rounded-xl
+                                opacity-0 group-hover:opacity-100
+                                transition-all duration-300
+                                whitespace-nowrap">
+
+                                    Delete Job
+
+                                    <!-- Tooltip Arrow -->
+                                    <span class="absolute left-full top-1/2 -translate-y-1/2 -translate-x-1/2
+                                    w-2.5 h-2.5 bg-gray-200
+                                    rotate-45"></span>
+                                </span>
+                            </div>
+                        </div>
                         <p class="designation font-light">${rejected.designation}</p>
                         <p class="salary font-light">${rejected.salary}</p>
-                        <button class="status bg-gray-100 p-2 font-bold">${rejected.status}</button>
+                        <button class="status bg-red-200 font-bold text-red-500 p-2 rounded">${rejected.status}</button>
                         <p class="details font-medium">${rejected.details}</p>
                         <div class="flex gap-2 mt-2">
                             <button
-                                class="interview-btn text-green-600 border border-green-600 text-lg py-2 px-2 rounded cursor-pointer">INTERVIEW</button>
+                                class="interview-btn text-lg py-2 px-2 rounded cursor-pointer                                 
+                                relative overflow-hidden  
+                                border border-green-500 text-green-600 font-semibold
+                                transition duration-300 
+                                before:absolute before:inset-0
+                              before:bg-green-100
+                                before:origin-left
+                                before:scale-x-0
+                                before:transition-transform
+                                before:duration-300
+                                hover:before:scale-x-100
+                                before:-z-10">INTERVIEW</button>
                             <button
-                                class="reject-btn text-red-600 border border-red-600 text-lg py-2 px-2 rounded cursor-pointer">REJECTED</button>
+                                class="reject-btn text-lg py-2 px-2 rounded cursor-pointer 
+                                relative overflow-hidden  
+                                border border-red-500 text-red-600 font-semibold
+                                transition duration-300 
+                                before:absolute before:inset-0
+                              before:bg-red-100
+                                before:origin-right
+                                before:scale-x-0
+                                before:transition-transform
+                                before:duration-300
+                                hover:before:scale-x-100
+                                before:-z-10
+                                ">REJECTED</button>
                         </div>
-                    </div>
-                    
-                     <div>
-                        <button
-                            class="delete-btn bg-red-500 text-white font-bold py-2 px-4 rounded cursor-pointer mt-4 md:mt-0">Delete</button>
                     </div>
     `;
         filteredJobsSection.appendChild(div);
